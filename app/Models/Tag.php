6ee118d31dadb6736205a26d6e9bd083dad69f31
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-03 16:23:21 +0330

    fill the models and also build relations

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable=[
+        'title',
+        'description',
+        'is_active',
+    ];
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
 }

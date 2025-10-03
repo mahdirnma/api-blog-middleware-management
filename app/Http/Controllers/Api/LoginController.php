49f31419bf21995c430c1d6da892301637ae6985
mahdirnma <rahnamamahdi78@gmail.com>
2025-10-03 18:20:36 +0330

    build api login process

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginUserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(LoginUserRequest $request)
     {
-        //
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json('Unauthorized', 401);
+        }
+        $token = Auth::user()->createToken('authToken')->plainTextToken;
+        return response()->json([
+            'token' => $token,
+            'user' => Auth::user()
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('categories',CategoryController::class);
-Route::apiResource('tags',TagController::class);
-Route::apiResource('posts',PostController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories',CategoryController::class);
+    Route::apiResource('tags',TagController::class);
+    Route::apiResource('posts',PostController::class);
+});
+Route::post('/auth',LoginController::class)->name('login');
